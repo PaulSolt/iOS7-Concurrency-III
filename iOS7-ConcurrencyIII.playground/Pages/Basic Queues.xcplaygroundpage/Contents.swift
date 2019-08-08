8d528d7eb7849706d0090c57672cce593270bb97
Paul Solt <PaulSolt@gmail.com>
2019-08-08 12:36:13 -0400

    Starting point for queues

--- iOS7-ConcurrencyIII.playground/Pages/Basic Queues.xcplaygroundpage/Contents.swift
+++ iOS7-ConcurrencyIII.playground/Pages/Basic Queues.xcplaygroundpage/Contents.swift
@@ -1,5 +1,28 @@
 import Foundation
 
-var str = "Hello, playground"
+let queue = DispatchQueue.global()
+
+print("1. I'm on the main thread")
+
+queue.async {
+    usleep(2_000_000) // 2 seconds (simulating work like downloading image files)
+    print("2. I'm in the background queue")
+}
+
+queue.sync {
+    usleep(3_000_000) // Pretend we're downloading an image
+    print("When does this happen?")
+}
+
+print("3. I'm not being blocked")
+
+queue.async {   // async means: don't wait to complete, start it sometime in the future
+    usleep(1_000_000) // 1 second
+    print("4. I'm doing more work in the background")
+}
+
+print("5. I'm not being blocked")
+
+
 
 //: [Next](@next)
